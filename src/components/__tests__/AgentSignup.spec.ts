298ee1525a08010446af6515fa91bb4db3b3bc28
mayurpatil96k <mayur1792001@gmail.com>
2024-07-19 15:51:20 +0530

    [Mayur]Add : Added new code for testing

---
+++ src/components/__tests__/AgentSignup.spec.ts
@@ -0,0 +1,22 @@
+import { describe, it, expect } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+import AgentSignup from '../Signup/AgentSignup.vue'
+import CustomerSignup from '../Signup/CustomerSignup.vue'
+
+describe('AgentSignup', () => {
+  it('renders properly', () => {
+    const wrapper = mount(AgentSignup)
+    expect(wrapper.exists()).toBe(true)
+  })
+})
+
+
+describe('CustomerSignup', () => {
+  it('renders properly', () => {
+    const wrapper = mount(CustomerSignup)
+    expect(wrapper.exists()).toBe(true)
+  })
+})
+
+
