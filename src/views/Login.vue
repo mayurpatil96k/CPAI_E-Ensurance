3c91950ee3b2a2fc2cb178174dadbdf4abcb4a80
Rajshingade7 <rajshingade777@gmail.com>
2024-07-19 12:24:49 +0530

    [Raj] add:added shims-vue.d.ts and login files

---
+++ src/views/Login.vue
@@ -0,0 +1,19 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+<template>
+  
+  <div class="mw-75 u-height  d-flex align-items-center justify-content-center">
+    <div class="mw-75 card">
+      <div class="card-body">
+        <RouterView />
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+.u-height{
+  min-height: 100vh;
+}
+</style>
