d61c608a7f73a2cf095019125d827db247f73182
mayurpatil96k <mayur1792001@gmail.com>
2024-07-20 15:09:13 +0530

    [Mayur]Add : Adding new files for Header

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light " style="background-color: #9c0516">
+  <div class="container-fluid">
+    <a class="navbar-brand u-color" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active u-color" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active u-color" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active u-color" aria-current="page" href="#">Home</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-light" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+</template>
+
+<style scoped>
+  .u-color{
+    color: white !important;
+  }
+</style>
--- src/components/Policies/MyPolicy.vue
+++ src/components/Policies/MyPolicy.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import PolicyCard from './PolicyCard.vue';
+import Header from '../Header.vue'
 </script>
 
 <template>
+    <Header/>
   <div class="container">
     <div class="row">
       <div class="col-12 col-sm-6 col-md-3 col-lg-3 mb-4" v-for="n in 20" :key="n">
@@ -14,8 +16,11 @@ import PolicyCard from './PolicyCard.vue';
   </div>
 </template>
 
-<style>
+<style scoped>
   .container {
     margin-top: 20px;
+  }
+  .u-color{
+    color: white !important;
   }
 </style>
