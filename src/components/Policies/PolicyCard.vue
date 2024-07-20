d11c726278e4cab99ce26b6cfdc9daf9437f02c8
mayurpatil96k <mayur1792001@gmail.com>
2024-07-20 13:48:55 +0530

    [Mayur]Add : Adding new feature for Policy Card

---
+++ src/components/Policies/PolicyCard.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="card border-danger border-2 hover-shadow" style="width: 18rem">
+      <img
+        src="https://life.futuregenerali.in/media/lzxjzwb3/life-insurance-for-you-and-your-family.webp"
+        class="card-img-top"
+        alt="LIC Plan Image"
+        
+      />
+      <div class="card-body">
+        <h5 class="card-title">LIC Jeevan Anand</h5>
+        <p class="card-text text-justify text-truncate-5">
+          LIC Jeevan Anand offers a combination of protection and savings. This plan provides
+          financial protection against death throughout the lifetime of the policyholder with the
+          provision of lump sum payment at the end of the selected policy term in case of survival.
+        </p>
+        <div class="d-flex justify-content-md-between">
+          <a href="#" class="btn btn-outline-danger">Buy Now</a>
+          <a href="#" class="btn btn-outline-danger">Know More</a>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <style>
+    .text-justify {
+      text-align: justify;
+    }
+    .text-truncate-5 {
+      display: -webkit-box;
+      -webkit-line-clamp: 5; 
+      -webkit-box-orient: vertical;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+    .hover-shadow:hover {
+      box-shadow: 0 4px 8px 0 rgba(220, 53, 69, 0.5), 0 6px 20px 0 rgba(220, 53, 69, 0.5);
+    }
+  </style>
+  
