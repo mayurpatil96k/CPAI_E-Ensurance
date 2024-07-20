60172cd4ed1c718fe07469f67eb700174c814819
mayurpatil96k <mayur1792001@gmail.com>
2024-07-20 11:48:04 +0530

    [Mayur]Add : Adding new test cases

--- src/components/__tests__/Admin_SignupTesting.spec.ts
+++ src/components/__tests__/Admin_SignupTesting.spec.ts
@@ -2,7 +2,6 @@ import { describe, it, expect,vi } from 'vitest'
 
 import { mount } from '@vue/test-utils'
 import AdminSignup from '../Signup/AdminSignup.vue'
-import AgentSignup from '../Signup/AgentSignup.vue'
 import EmployeeSignup from '../Signup/EmployeeSignup.vue'
 
 vi.mock('@vueform/vueform', () => ({
@@ -18,20 +17,15 @@ vi.mock('@vueform/vueform', () => ({
 
 describe('AdminSignup', () => {
   it('renders properly', () => {
-    const wrapper = mount(AdminSignup, { props: { msg: 'Hello just testing the component is rendering or not' } })
+    const wrapper = mount(AdminSignup)
     expect(wrapper.exists()).toBe(true)
   })
+  
 })
 describe('EmployeeSignup', () => {
   it('renders properly', () => {
-    const wrapper = mount(EmployeeSignup, { props: { msg: 'Hello just testing the component is rendering or not' } })
+    const wrapper = mount(EmployeeSignup)
     expect(wrapper.exists()).toBe(true)
   })
 })
 
-
-// describe('agentSignup', () => {
-//   it('renders properly', () => {
-//     const wrapper = mount(AgentSignup, { props: { msg: 'Hello just testing the component is rendering or not' } })
-//   })
-// })
