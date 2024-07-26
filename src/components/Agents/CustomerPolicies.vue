c32f0c154414f14d7ed5b6dce41d73365bcdf8e4
Rajshingade7 <rajshingade777@gmail.com>
2024-07-26 14:44:52 +0530

    [Raj] Mod:modified the css for editemployee

--- src/components/Agents/CustomerPolicies.vue
+++ src/components/Agents/CustomerPolicies.vue
@@ -244,9 +244,6 @@ export default {
                   <td>
                     <ul class="action-list">
                       <li>
-                        <a href="#" @click.prevent="viewcustomerdetails" data-tip="view"><i class="fa fa-eye"></i></a>
-                      </li>
-                      <li>
                         <a href="#" @click.prevent="confirmDelete(customer)" data-tip="delete"><i class="fa fa-trash"></i></a>
                       </li>
                     </ul>
--- src/components/Agents/EditEmployee.vue
+++ src/components/Agents/EditEmployee.vue
@@ -72,7 +72,7 @@ export default defineComponent({
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
+  background-color: transparent;
   display: flex;
   justify-content: center;
   align-items: center;
